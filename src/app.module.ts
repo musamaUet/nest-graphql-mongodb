1d49513e28e0143c2c3a57888e910c95051252e8
Muhammad Usama Rabani <usamaitians.gcuf@gmail.com>
2023-07-08 22:11:33 +0500

    graphql setup (#1)
    Co-authored-by: musamauet <usamarabani@renesistech.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
-// import {} from '@nestjs'
+import { GraphQLModule } from '@nestjs/graphql';
+import { LessonModule } from './lesson/lesson.module';
+import { ApolloDriver } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+      driver: ApolloDriver,
+    }),
+    LessonModule,
+  ],
 })
 export class AppModule {}
